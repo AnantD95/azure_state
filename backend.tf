2388f27839a0fe62b0fb54763eacacfe5c949f09
AnantD95 <59355088+AnantD95@users.noreply.github.com>
2024-03-22 13:15:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "tfstoragestorestate"
-    container_name       = "tfstoragecontainertest"
+    storage_account_name = ""tfrgststate""
+    container_name       = ""tfrgstcontainer""
     key                  = "terraform.tfstate"
     # Specify the access key for authentication
     access_key           = "LLrulFixmmUlnP5zg/n1lrJVSP46NEs1dSmb40H0Dz9KEn8CXiLdMWSb9Bqqrssn3SlNB6SIAXAl+AStUZ4hHw=="
