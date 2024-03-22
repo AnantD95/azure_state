f9072c80341a57aeaf6e38ff5a0c3d577a6492e3
AnantD95 <59355088+AnantD95@users.noreply.github.com>
2024-03-22 14:43:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-# terraform {
-#   backend "azurerm" {
-#     storage_account_name = var.storage_account_name
-#     container_name       = var.container_name
-#     key                  = "terraform.tfstate"
-#   }
-# }
-
 provider "azurerm" {
   features {}
 }
